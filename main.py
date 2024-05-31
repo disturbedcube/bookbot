f3f87dc93cb55ae18b9ba2beb21daedd50e7c587
Ericson Etienne Jr. <eletiennejr@gmail.com>
2024-05-31 10:07:04 -0400

    Completed project, generate report function added.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 def main():
-    book_path ="books/frankenstein.txt"
-    book_text = get_text(book_path)
-    words_in_text = count_words(book_text)
-    letters_in_text = count_letters(book_text)
-    print(f"There are {words_in_text} words in the book found at the path: {book_path}, the number of iterations of each character is as follows: {letters_in_text}")
+
+    generate_report()
+    
 def get_text(path):
+
     with open(path) as f:
         return f.read()
     
 def count_words(book):
+
     words_in_book = book.split()
     return len(words_in_book)
 
 def count_letters(book):
+
     letters ={}
     words = book.split()
     for word in words:
@@ -24,4 +25,27 @@ def count_letters(book):
                     letters[char.lower()] = 1
     return letters
 
+def sort_on(dict):
+
+    return dict["value"]
+
+def generate_report():
+
+    book_path ="books/frankenstein.txt"
+    book_text = get_text(book_path)
+    words_in_text = count_words(book_text)
+    letters_in_text = count_letters(book_text)
+    list_of_characters = []
+    for key in letters_in_text:
+        list_of_characters.append({"key":key, "value":letters_in_text[key]})
+    list_of_characters.sort(reverse=True,key=sort_on)
+
+    print("Generating Report \n")
+    print("--------------------------------------------")
+    print(f"There are {words_in_text} words in the book found at the path: {book_path} \n")
+    for dictionary in list_of_characters:
+        print(f"The letter {dictionary["key"]} was found {dictionary["value"]} times. \n")
+    print("--------------------------------------------")
+
+    
 main()
